a5c123946c7d127ee4828136b29a2998feedab3b
Alex Regier <alexander.regier@simpleclub.com>
2025-05-13 17:42:36 +0200

    fix: Upgrade to latest firebase extension

--- functions/src/shipToElastic.ts
+++ functions/src/shipToElastic.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import { firestore, logger, Change } from "firebase-functions/v1";
 import { toAppSearch } from "./toAppSearch";
 
 import { getNewAppSearchClient } from "./utils";
@@ -7,14 +7,12 @@ const appSearchClient = getNewAppSearchClient();
 
 // We separate and curry this function from shipToElastic so we can test with less mocking
 export const handler = (client: any) => {
-  return async (
-    change: functions.Change<functions.firestore.DocumentSnapshot>
-  ) => {
-    functions.logger.info(`Received request to ship to ship to Elastic`, {
+  return async (change: Change<firestore.DocumentSnapshot>) => {
+    logger.info(`Received request to ship to ship to Elastic`, {
       change,
     });
     if (change.before.exists === false) {
-      functions.logger.info(`Creating document`, { id: change.after.id });
+      logger.info(`Creating document`, { id: change.after.id });
       try {
         client.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
           {
@@ -23,25 +21,25 @@ export const handler = (client: any) => {
           },
         ]);
       } catch (e) {
-        functions.logger.error(`Error while creating document`, {
+        logger.error(`Error while creating document`, {
           id: change.after.id,
         });
         throw e;
       }
     } else if (change.after.exists === false) {
-      functions.logger.info(`Deleting document`, { id: change.before.id });
+      logger.info(`Deleting document`, { id: change.before.id });
       try {
         client.destroyDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
           change.before.id,
         ]);
       } catch (e) {
-        functions.logger.error(`Error while deleting document`, {
+        logger.error(`Error while deleting document`, {
           id: change.before.id,
         });
         throw e;
       }
     } else {
-      functions.logger.info(`Updating document`, { id: change.after.id });
+      logger.info(`Updating document`, { id: change.after.id });
       try {
         client.indexDocuments(process.env.APP_SEARCH_ENGINE_NAME, [
           {
@@ -50,7 +48,7 @@ export const handler = (client: any) => {
           },
         ]);
       } catch (e) {
-        functions.logger.error(`Error while updating document`, {
+        logger.error(`Error while updating document`, {
           id: change.after.id,
         });
         throw e;
@@ -61,9 +59,8 @@ export const handler = (client: any) => {
 };
 
 // Note that in extensions, functions get declared slightly differently then typical extensions:
-// https://firebase.google.com/docs/extensions/alpha/construct-functions#firestore
-// Also note that tyipcally in a function you specify the path in the call to `document` like `/${config.collectionName}/{documentId}`.
-// In an extension, the path is specified in extension.yaml, in eventTrigger.
-export const shipToElastic = functions.handler.firestore.document.onWrite(
-  handler(appSearchClient)
-);
+// https://firebase.google.com/docs/extensions/publishers/functions#firestore
+// See also https://firebase.google.com/docs/extensions/publishers/functions#special-considerations for the path to be defined here.
+export const shipToElastic = firestore
+  .document("{collection_id}/{doc_id}")
+  .onWrite(handler(appSearchClient));
